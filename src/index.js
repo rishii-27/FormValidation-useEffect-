bd3ff7123ca9d8320f1246838bb29635cda2cd49
rishii-27 <hrishikesh.joshi1997@gmail.com>
2023-05-03 15:51:05 +0530

    Replacing all props using useContext

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-
 import './index.css';
 import App from './App';
+import { AuthcontextProvider } from './store/auth-context';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);
+root.render(<AuthcontextProvider>
+    <App />
+</AuthcontextProvider>);
